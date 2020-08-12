2189c9884077c931197f7764e2cc688391d7d15a
1162kota <s1832076sf@s.chibakoudai.jp>
2020-08-12 18:39:22 +0900

    report
    修正１

--- newton.js
+++ newton.js
@@ -3,8 +3,8 @@ window.addEventListener("load", () => {
  var mui = new nibun_ui(
  document.querySelector("#start"),
  document.querySelector("#clear"),
- document.querySelector("#graph1"),
- document.querySelector("#graph1b")
+ document.querySelector("#graph2"),
+ document.querySelector("#graph2b")
  );
  mui.setNumber_ui(document.querySelectorAll("#panel2 > button"));
  mui.setResult_ui(document.querySelector("#result"));
@@ -23,12 +23,7 @@ class nibun_ui {
       });
       this.start.addEventListener("click", () => {
         let result = this.nibun.next();
-        this.result.textContent =
-          "計算結果：" +
-          result[0] / result[1] * 4.0 +
-          "(試行回数：" +
-          result[1] +
-          "回)";
+        this.result.textContent =0 //未完成
       });
     }
     setNumber_ui(btnArray) {
@@ -45,48 +40,24 @@ class nibun_ui {
     }
     draw_axis() {
     }
-  }
-  class nibun {
-    constructor(canvas) {
-      this.canvas = canvas;
-      this.ctx = canvas.getContext("2d");
-      this.times = 10000; // startボタンが押された際の打点数
-      this.all_number = 0;  // 全打点数（累積)
-      this.inside_number = 0; // 円内に入った点数(累積)
-    }
-    next() {
-      for (let i = 0; i < this.times; i++) {
-        // Math.randomの返り値は0.0〜1.0
-        // これを-1.0〜1.0に整える
-        let x = Math.random() * 2.0 - 1.0;
-        let y = Math.random() * 2.0 - 1.0;
-  
-        let length = Math.sqrt(x * x + y * y);
-        if (length <= 1.0) {
-          this.ctx.strokeStyle = "rgb(40, 40, 200)";
-          this.inside_number++;
-        } else {
-          this.ctx.strokeStyle = "rgb(220, 40, 40)";
-        }
-        this.all_number++;
-  
-        this.ctx.beginPath();
-        // -1.0〜1.0の座標を0〜横幅（または高さ）に整える
-        let px = (x + 1.0) * this.canvas.width / 2.0;
-        let py = (y + 1.0) * this.canvas.height / 2.0;
-        this.ctx.moveTo(px, py);
-        this.ctx.lineTo(px + 1, py + 1);
-        this.ctx.stroke();
-      }
-      return [this.inside_number, this.all_number];
-    }
-    clearCanvas() {
-      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      this.inside_number = 0;
-      this.all_number = 0;
-    }
-    setTimes(value) {
-      this.times = value;
-    }
-  }
-  
+}
+class nibun {
+constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+    this.times = 10000; // startボタンが押された際の打点数
+    this.all_number = 0;  // 全打点数（累積)
+    this.inside_number = 0; // 円内に入った点数(累積)
+}
+next() {
+
+}
+clearCanvas() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.inside_number = 0;
+    this.all_number = 0;
+}
+setTimes(value) {
+    this.times = value;
+}
+}
